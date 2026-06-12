7bdf600244157f30966db18d0ed187ce274cf903
phemlogs <phemlogs@gmail.com>
2026-06-12 09:49:22 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Paste your deployed Apps Script Web App URL here.
 // It should look like: https://script.google.com/macros/s/AKfycb.../exec
-const SCRIPT_URL = "YOUR_APPS_SCRIPT_WEB_APP_URL_HERE";
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwDDaivHottiI2AgcTZLl3Y6HYnVbaK9D7LKxjznXMyDbOmq3VAAhm4bc6bdWqIDsMw/exec";
